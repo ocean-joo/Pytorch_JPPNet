2ebc9656b15a7a707ae6b09d809bf95c06dda582
MinYoungLee <21400526@gmail.com>
2019-08-08 02:41:18 +0900

    Evaluate_Pose_JPPNET.py: Modified logic

--- Evaluate_Pose_JPPNET.py
+++ Evaluate_Pose_JPPNET.py
@@ -1,21 +1,29 @@
 import torch
 import torch.nn as nn
-import numpy as np
-from LIP_Model import PoseRefineNet, ParsingRefineNet, PoseNet
+import torchvision.transforms as transforms
+from PIL import Image
 
-img = Image.open(img_path)
-transform = torch.Compose([
-    torch.ToTensor()])
-img = transform(img)
+from LIP_Model import PoseRefineNet, ParsingRefineNet, PoseNet, ParsingNet
+from ResNet_extract import BackboneResNet
+from Model import JPPNetModel
 
-
+parsing_net = ParsingNet(20)
 pose_net = PoseNet()
-pose_refine = PoseRefineNet()
-parsing_refine = ParsingRefineNet()
+pose_refine = PoseRefineNet(20)
+parsing_refine = ParsingRefineNet(20)
+model = JPPNetModel()
+#model = BackboneResNet()
 
-parsing_fea1, parsing_out1, resnet_fea1 = JPPNetModel([img])
-pose_map1, pose_context1 = pose_net(resnet_fea1)
+img = Image.open("2583_462340.jpg")
+transform = transforms.Compose([transforms.ToTensor()])
+img = transform(img)
+img = img.unsqueeze(0)
+
+feature_map = model(img)
+parsing_out1, parsing_fea1 = parsing_net(feature_map)
+#resnet_fea1, parsing_out1, parsing_fea1 = model(img)
+
+pose_map1, pose_context1 = pose_net(feature_map)
 pose_map2, pose_context2 = pose_refine(pose_map1, parsing_out1, pose_context1)
 parsing_out2, parsing_fea2 = parsing_refine(pose_map1, parsing_out1, parsing_fea1)
 pose_map3, pose_context3 = pose_refine(pose_map2, parsing_out2, pose_context2)
-
