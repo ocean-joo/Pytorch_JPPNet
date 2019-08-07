d5cb4720e5e1549ac951e4352a2c8b4a11303e91
MinYoungLee <21400526@gmail.com>
2019-08-07 19:42:42 +0900

    Model.py : Added ParsingRefineNet

--- Model.py
+++ Model.py
@@ -3,24 +3,21 @@ from .ResNet_extract import *
 from .utils import *
 
 class PoseRefineNet(nn.Module) :
-    def __init__(self, input_) :
+    def __init__(self, num_classes) :
         super(PoseRefineNet, self).__init__()
         ## input size 확인
         
         # 1*1 Convolution remaps the heatmaps to match the number of channels of the intermediate features
         self.pose_remap = nn.Sequential(
-                nn.Conv2d(pose_size, 128, 1, 1),
+                nn.Conv2d(16, 128, 1, 1),
                 nn.ReLU())
         self.parsing_remap = nn.Sequential(
-                nn.Conv2d(parsing_size, 128, 1, 1),
+                nn.Conv2d(num_classes, 128, 1, 1),
                 nn.ReLU())
-
-        # Concat two remapped feature maps
-
 
         # Get pose context 2
         self.pose_refine1 = nn.Sequential(
-                nn.Conv2d(concat_size, 512, 3, 1),
+                nn.Conv2d(512, 512, 3, 1),
                 nn.ReLU())
         self.pose_refine2 = nn.Sequential(
                 nn.Conv2d(512, 256, 5, 1),
@@ -44,16 +41,71 @@ class PoseRefineNet(nn.Module) :
         parsing_out = self.parsing_remap(parsing_map)
 
         # Concat two remapped feature maps and pose_context
-        pose_out = nn.
+        pose_out = torch.cat(pose_out, parsing_out, pose_context)
 
-        return output, feature
+        pose_out = self.pose_refine1(pose_out)
+        pose_out = self.pose_refine2(pose_out)
+        pose_out = self.pose_refine3(pose_out)
+        pose_context = self.pose_refine4(pose_out)
+        pose_out = self.pose_refine5(pose_context)
+        pose_out = self.pose_refine6(pose_out)
+
+        return pose_out, pose_context
 
 class ParsingRefineNet(nn.Module) :
-    def __init__(self) :
+    def __init__(self, num_classes) :
         super(ParsingRefineNet, self).__init__()
 
-    def forward(self, x) :
+        # 1*1 Convolution remaps the heatmaps to match the number of channels of the intermediate features
+        self.pose_remap = nn.Sequential(
+                nn.Conv2d(16, 128, 1, 1),
+                nn.ReLU())
+        self.parsing_remap = nn.Sequential(
+                nn.Conv2d(num_classes, 128, 1, 1),
+                nn.ReLU())
 
+        # Get parsing context 2
+        self.parsing_refine1 = nn.Sequential(
+                nn.Conv2d(512, 512, 3, 1),
+                nn.ReLU())
+        self.parsing_refine2 = nn.Sequential(
+                nn.Conv2d(512, 256, 5, 1),
+                nn.ReLU())
+        self.parsing_refine3 = nn.Sequential(
+                nn.Conv2d(256, 256, 7, 1),
+                nn.ReLU())
+        self.parsing_refine4 = nn.Sequential(
+                nn.Conv2d(256, 256, 9, 1),
+                nn.ReLU())
+
+        # Get parsing map 2
+        self.parsing_refine5 = nn.Sequential(
+                nn.Conv2d(256, 256, 1, 1),
+                nn.ReLU())
+        self.parsing_atrous1 = nn.Conv2d(256, 20, 3, dilation=6)
+        self.parsing_atrous2 = nn.Conv2d(256, 20, 3, dilation=12)
+        self.parsing_atrous3 = nn.Conv2d(256, 20, 3, dilation=18)
+        self.parsing_atrous4 = nn.Conv2d(256, 20, 3, dilation=24)
+
+    def forward(self, pose_map, parsing_map, parsing_context) :
+        pose_out = self.pose_remap(pose_map)
+        parsing_out = self.parsing_remap(parsing_map)
+
+        # Concat two remapped feature maps and parsing_context
+        parsing_out = torch.cat(pose_out, parsing_out, parsing_context)
+
+        parsing_out = self.parsing_refine1(parsing_out)
+        parsing_out = self.parsing_refine2(parsing_out)
+        parsing_out = self.parsing_refine3(parsing_out)
+        parsing_context = self.parsing_refine4(parsing_out)
+        parsing_out = self.parsing_refine5(parsing_context) 
+        parsing_human1 = self.parsing_atrous1(parsing_out)
+        parsing_human2 = self.parsing_atrous2(parsing_out)
+        parsing_human3 = self.parsing_atrous3(parsing_out)
+        parsing_human4 = self.parsing_atrous4(parsing_out)
+        parsing_map = parsing_human1 + parsing_human2 + parsing_human3 + parsing_human4
+
+        return parsing_map, parsing_context
 
 class PoseNet(nn.Module) :
     def __init__(self) :
@@ -61,7 +113,7 @@ class PoseNet(nn.Module) :
         
         ## Get pose context 1
         self.pose_conv1 = nn.Sequential(
-                nn.Conv2d(input_size, 512, 3, 1),
+                nn.Conv2d(1024, 512, 3, 1),
                 nn.ReLU())
         self.pose_conv2 = nn.Sequential(
                 nn.Conv2d(512, 512, 3, 1),
@@ -100,8 +152,10 @@ class PoseNet(nn.Module) :
         return pose_out, pose_context
 
 class ParsingNet(nn.Module) :
-    def __init__(self) :
+    def __init__(self, num_classes) :
         super(ParsingNet, self).__init()
+        
+        self.parsing_
 
     def forward(self, x) :
         pass
